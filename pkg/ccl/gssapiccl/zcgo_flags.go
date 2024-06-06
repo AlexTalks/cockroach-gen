0135d78f6fa0e4273b43c36158f6db741f842568
Alex Sarkesian <alex.sarkesian@temporal.io>
2024-06-05 18:37:38 -0700

    *: adding generated files
    Release note: None

---
+++ pkg/ccl/gssapiccl/zcgo_flags.go
@@ -0,0 +1,7 @@
+// GENERATED FILE DO NOT EDIT
+
+package gssapiccl
+
+// #cgo CPPFLAGS: -I/Users/asarkesian/go/src/github.com/cockroachdb/cockroach/bin/c-deps/archived_cdep_libjemalloc_macosarm/include
+// #cgo LDFLAGS: -L/Users/asarkesian/go/src/github.com/cockroachdb/cockroach/bin/c-deps/archived_cdep_libjemalloc_macosarm/lib -L/Users/asarkesian/go/src/github.com/cockroachdb/cockroach/bin/c-deps/archived_cdep_libproj_macosarm/lib
+import "C"
